92849decca1e6108e52be43d32f63faa0f357244
laiane <laiane@laiane-POSITIVO-MOBILE>
2016-03-14 17:24:18 -0300

    commit eventos do coordenador 14/03/2016

---
+++ https:/github.com/Laiane7771/Eventif-Projeto.git/Eventif-Site/src/edu/ifg/formosa/coordenador/event/CadastrarAtividadeEventHandler.java
@@ -0,0 +1,7 @@
+package edu.ifg.formosa.coordenador.event;
+
+import com.google.gwt.event.shared.EventHandler;
+
+public interface CadastrarAtividadeEventHandler extends EventHandler{
+	void cadastrarAtividade(CadastrarAtividadeEvent event);
+}
---
+++ https:/github.com/Laiane7771/Eventif-Projeto.git/Eventif-Site/src/edu/ifg/formosa/coordenador/event/PesquisarAtividadeEventHandler.java
@@ -0,0 +1,7 @@
+package edu.ifg.formosa.coordenador.event;
+
+import com.google.gwt.event.shared.EventHandler;
+
+public interface PesquisarAtividadeEventHandler extends EventHandler{
+	void pesquisarAtividade(PesquisarAtividadeEvent event);
+}
